2eff8f8bb2223d4324ef18d9b1cbd20da4ae2efe
Luke Schula <schulaluke@gmail.com>
2024-03-15 11:58:26 -0500

    check, nothing displayed

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""export data in the CSV format"""
+
+from sys import argv
+from requests import get
+
+link = "https://jsonplaceholder.typicode.com/"
+
+
+def export_data_CSV():
+    employee = get(link + argv[1]).json()
+    tasks = get(link + argv[1] + '/todos').json()
+    convert = argv[1] + '.csv'
+
+    for task in tasks:
+        display = '"' + str(employee['id']) + '",' + '"' + employee['username'] + '",' +\
+        '"' + str(task['completed']) + '",' + '"' + task['title'] +\
+        '"\n'
+
+        with open(convert, 'a', encoding='uft-8') as f:
+            f.write(display)
+
+if __name__ == '__main__':
+    export_data_CSV()
+
+
